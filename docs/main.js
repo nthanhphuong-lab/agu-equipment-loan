45dff69e912fde7f34ff9e50192f6270bfbdb9cb
nthanhphuong-lab <nthanhphuong@agu.edu.vn>
2025-11-18 10:16:32 +0700

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -1051,21 +1051,6 @@ function formatDate(timestamp) {
 
 
 
-// ================== HELPER ==================
-function formatDate(timestamp) {
-  if (!timestamp || !timestamp.toDate) return "(Không có)";
-  const date = timestamp.toDate();
-  const day = String(date.getDate()).padStart(2,'0');
-  const month = String(date.getMonth()+1).padStart(2,'0');
-  const year = date.getFullYear();
-  let hours = date.getHours();
-  const minutes = String(date.getMinutes()).padStart(2,'0');
-  const seconds = String(date.getSeconds()).padStart(2,'0');
-  const ampm = hours >= 12 ? "PM" : "AM";
-  hours = hours % 12 || 12;
-  hours = String(hours).padStart(2,'0');
-  return `${day}/${month}/${year}, ${hours}:${minutes}:${seconds} ${ampm}`;
-}
 
 // ================== STATUS MAP ==================
 const statusMap = {
